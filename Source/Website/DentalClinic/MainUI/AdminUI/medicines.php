e0ab40dfc9f9eed1fea58547e97e332c8894f7b9
CatLe2003 <119800521+CatLe2003@users.noreply.github.com>
2023-12-20 00:39:45 +0700

    display Medicine list with paging function

--- Source/Website/DentalClinic/MainUI/AdminUI/medicines.php
+++ Source/Website/DentalClinic/MainUI/AdminUI/medicines.php
@@ -1,10 +1,11 @@
 <?php
-// session_start();
-// include('config/config.php');
-// include('config/checklogin.php');
-// check_login();
-require_once('./partials/_head.php');
-// require_once('./partials/_analytics.php');
+    $page_title = "Smile - Staff List";
+    require_once('./partials/_head.php');
+
+    $pageSize = 100;
+    $pageNumber = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+
+    $medicines = getAllWithPagination('MEDICINE', $pageSize, $pageNumber, 'ID_Medicine');
 ?>
 
 <body>
@@ -28,6 +29,32 @@ require_once('./partials/_head.php');
                                 <i class="fa-solid fa-pills btn-control-icon"></i>
                                 Add new medicine
                             </a>
+                            <div class="pagination">
+                                <?php
+                                    $totalPages = ceil($medicines['total'] / $pageSize);
+                                    $maxPagesToShow = 4;
+                                    $halfMax = floor($maxPagesToShow / 2);
+
+                                    // Hiển thị nút Previous
+                                    if ($pageNumber > 1) {
+                                        echo '<a href="?page=' . ($pageNumber - 1) . '">&laquo;</a>';
+                                    } else {
+                                        echo '<a class="disabled" href="#">&laquo;</a>';
+                                    }
+
+                                    // Hiển thị các nút trang
+                                    for ($i = max(1, $pageNumber - $halfMax); $i <= min($totalPages, $pageNumber + $halfMax); $i++) {
+                                        echo '<a class="' . ($i == $pageNumber ? 'active' : '') . '" href="?page=' . $i . '">' . $i . '</a>';
+                                    }
+
+                                    // Hiển thị nút Next
+                                    if ($pageNumber < $totalPages) {
+                                        echo '<a href="?page=' . ($pageNumber + 1) . '">&raquo;</a>';
+                                    } else {
+                                        echo '<a class="disabled" href="#">&raquo;</a>';
+                                    }
+                                ?>
+                            </div>
                             <div class="container__heading-search">
                                 <input type="text" class="heading-search__area" placeholder="Search by code, name..." name="search_text">
                                 <button class="btn-control btn-control-search" name="btn-search">
@@ -49,27 +76,75 @@ require_once('./partials/_head.php');
                                     </tr>
                                 </thead>
                                 <tbody class="table-body">
+                                <tbody class="table-body">
+                                <?php
+                                    $count = sizeof($medicines['data']);
+                                    //echo $staffs['data'];
+                                    if($count > 0)
+                                    {
+                                    ?>
+                                        <?php  foreach($medicines['data'] as $medicine) 
+                                        {  
+                                        ?>
                                     <tr>
-                                        <th class="text-column-emphasis" scope="row">1</th> 
-                                        <th class="text-column" scope="row">bla bla</th> 
-                                        <th class="text-column" scope="row">12</th> 
+                                        <th class="text-column-emphasis" scope="row"><?php echo $medicine['ID_Medicine']?></th>
+                                        <th class="text-column" scope="row"><?php echo $medicine['MedicineName']?></th>
+                                        <th class="text-column" scope="row"><?php echo $medicine['Price']?></th> 
+ 
                                         <th class="text-column" scope="row">
                                             <div class="text-column__action">
-                                                <button class="btn-control btn-control-delete" name="btn-delete">
-                                                    <i class="fa-solid fa-trash-can btn-control-icon"></i>
-                                                    Delete
-                                                </button>
-                                                <a href="update_medicines.php" class="btn-control btn-control-edit">
-                                                    <i class="fa-solid fa-pills btn-control-icon"></i>
+                                                <a href="../../Controller/AdminController/delete_medicine.php?id=<?php  echo $medicine['ID_Medicine']?>" 
+                                                    class="btn-control btn-control-delete">
+                                                        <i class="fa-solid fa-trash-can btn-control-icon"></i>
+                                                        Delete
+                                                </a>
+                                                <a href="update_medicines.php?id=<?php  echo $medicine['ID_Medicine']?>" class="btn-control btn-control-edit">
+                                                    <i class="fa-solid fa-user-pen btn-control-icon"></i>
                                                     Update
                                                 </a>
                                             </div>
                                         </th> 
                                     </tr>
+                                    <?php
+                                        }
+                                    }
+                                    else
+                                    {?>
+                                       <th class="text-column" scope="row"><?php echo 'No Data Found'?></th> 
+                                    <?php    
+                                    }
+                                    ?>
 
                                 </tbody>
                             </table>
+                            <!-- <div class="pagination-container">
+                                <ul class="pagination">
+                                    <?php
+                                        // $totalPages = ceil($medicines['total'] / $pageSize);
+                                        // $maxPagesToShow = 4;
+                                        // $halfMax = floor($maxPagesToShow / 2);
 
+                                        // // Hiển thị nút Previous
+                                        // if ($pageNumber > 1) {
+                                        //     echo '<li><a href="?page=' . ($pageNumber - 1) . '">&laquo; Previous</a>';
+                                        // } else {
+                                        //     echo '<li class="disabled"><span>&laquo; Previous</span>';
+                                        // }
+
+                                        // // Hiển thị các nút trang
+                                        // for ($i = max(1, $pageNumber - $halfMax); $i <= min($totalPages, $pageNumber + $halfMax); $i++) {
+                                        //     echo '<li class="' . ($i == $pageNumber ? 'active' : '') . '"><a href="?page=' . $i . '">' . $i . '</a></li>';
+                                        // }
+
+                                        // // Hiển thị nút Next
+                                        // if ($pageNumber < $totalPages) {
+                                        //     echo '<a href="?page=' . ($pageNumber + 1) . '">Next &raquo;</a>';
+                                        // } else {
+                                        //     echo '<a class="disabled"><span>Next &raquo;</span></a>';
+                                        // }
+                                    ?>
+                                </ul>
+                            </div> -->
                         </div>
                     </div>
                 </div>
