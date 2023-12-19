41c8a65b3398102267e61604f49718ad857fa537
CatLe2003 <119800521+CatLe2003@users.noreply.github.com>
2023-12-20 00:40:20 +0700

    Display staff list dynamically

--- Source/Website/DentalClinic/MainUI/AdminUI/staffs.php
+++ Source/Website/DentalClinic/MainUI/AdminUI/staffs.php
@@ -3,8 +3,10 @@
 // include('config/config.php');
 // include('config/checklogin.php');
 // check_login();
+$page_title = "Smile - Staff List";
 require_once('./partials/_head.php');
 // require_once('./partials/_analytics.php');
+    $staffs = getbyUserType('USER_DENTAL', 'Staff');
 ?>
 
 <body>
@@ -24,7 +26,7 @@ require_once('./partials/_head.php');
                 <div class="container-recent">
                     <div class="container-recent-inner">
                         <div class="container-recent__heading heading__button">
-                            <a href="add_staffs.html" class="btn-control btn-control-add">
+                            <a href="add_staffs.php" class="btn-control btn-control-add">
                                 <i class="fa-solid fa-user-plus btn-control-icon"></i>
                                 Add new staff
                             </a>
@@ -41,23 +43,44 @@ require_once('./partials/_head.php');
                             <table class="table">
                                 <thead class="thead-light">
                                     <tr>
-                                        <th class="text-column-emphasis" scope="col">ID STAFF</th> 
+                                        <th class="text-column-emphasis" scope="col">ID</th> 
+                                        <th class="text-column" scope="col">Username</th> 
                                         <th class="text-column" scope="col">FULL NAME</th> 
                                         <th class="text-column" scope="col">Gender</th> 
                                         <th class="text-column" scope="col">Phone Number</th> 
                                         <th class="text-column" scope="col">Address</th> 
-                                        <th class="text-column" scope="col">DOB</th> 
+                                        <!-- <th class="text-column" scope="col">DOB</th>  -->
                                         <th class="text-column" scope="col">ACTION</th> 
                                     </tr>
                                 </thead>
                                 <tbody class="table-body">
+                                <?php
+                                    $count = sizeof($staffs['data']);
+                                    //echo $staffs['data'];
+                                    if($count > 0)
+                                    {
+                                    ?>
+                                        <?php  foreach($staffs['data'] as $staff) 
+                                        {  
+                                        ?>
                                     <tr>
-                                        <th class="text-column" scope="row">3434</th> 
-                                        <th class="text-column" scope="row">Sarah</th> 
-                                        <th class="text-column" scope="row">Female</th> 
-                                        <th class="text-column" scope="row">0923838456</th> 
-                                        <th class="text-column" scope="row">District 5, HCM</th> 
-                                        <th class="text-column" scope="row">23/1/2001</th> 
+                                        <th class="text-column-emphasis" scope="row"><?php echo $staff['ID_User']?></th>
+                                        <th class="text-column" scope="row"><?php echo $staff['Username']?></th>
+                                        <th class="text-column" scope="row"><?php echo $staff['Fullname']?></th> 
+                                        <?php if($staff['Gender'] = 'F')
+                                            {?>
+                                                <th class="text-column" scope="row">Female</th> 
+                                            <?php
+                                            }
+                                            else
+                                            {
+                                            ?>
+                                                <th class="text-column" scope="row">Male</th> 
+                                            <?php
+                                            }
+                                        ?>
+                                        <th class="text-column" scope="row"><?php echo $staff['PhoneNumber']?></th> 
+                                        <th class="text-column" scope="row"><?php echo $staff['CurrAddress']?></th> 
                                         <th class="text-column" scope="row">
                                             <div class="text-column__action">
                                                 <button class="btn-control btn-control-delete" name="btn-delete">
@@ -71,6 +94,15 @@ require_once('./partials/_head.php');
                                             </div>
                                         </th> 
                                     </tr>
+                                    <?php
+                                        }
+                                    }
+                                    else
+                                    {?>
+                                       <th class="text-column" scope="row"><?php echo 'No Data Found'?></th> 
+                                    <?php    
+                                    }
+                                    ?>
                                 </tbody>
                             </table>
 
