275befd21acdc426384287b53839d0b65406e9f1
BNKieuNhi <93073321+BNKieuNhi@users.noreply.github.com>
2023-12-20 14:28:19 +0700

    Update staffs.php

--- Source/Website/DentalClinic/MainUI/AdminUI/staffs.php
+++ Source/Website/DentalClinic/MainUI/AdminUI/staffs.php
@@ -3,8 +3,10 @@
 // include('config/config.php');
 // include('config/checklogin.php');
 // check_login();
+$page_title = "Smile - Staff List";
 require_once('./partials/_head.php');
 // require_once('./partials/_analytics.php');
+    $staffs = getbyUserType('USER_DENTAL', 'Staff');
 ?>
 
 <body>
@@ -24,9 +26,9 @@ require_once('./partials/_head.php');
                 <div class="container-recent">
                     <div class="container-recent-inner">
                         <div class="container-recent__heading heading__button">
-                            <a href="add_dentists.php" class="btn-control btn-control-add">
+                            <a href="add_staffs.php" class="btn-control btn-control-add">
                                 <i class="fa-solid fa-user-plus btn-control-icon"></i>
-                                Add new dentist
+                                Add new staff
                             </a>
                             <div class="container__heading-search">
                                 <input type="text" class="heading-search__area" placeholder="Search by code, name..." name="search_text">
@@ -34,7 +36,6 @@ require_once('./partials/_head.php');
                                     <i class="fa-solid fa-magnifying-glass btn-control-icon"></i>
                                     Search
                                 </button>                        
-
                             </div>
                         </div>
 
@@ -43,7 +44,7 @@ require_once('./partials/_head.php');
                                 <thead class="thead-light">
                                     <tr>
                                         <th class="text-column-emphasis" scope="col">ID</th> 
-                                        <th class="text-column" scope="col">USERNAME</th> 
+                                        <th class="text-column" scope="col">Username</th> 
                                         <th class="text-column" scope="col">FULL NAME</th> 
                                         <th class="text-column" scope="col">Gender</th> 
                                         <th class="text-column" scope="col">Phone Number</th> 
@@ -53,27 +54,55 @@ require_once('./partials/_head.php');
                                     </tr>
                                 </thead>
                                 <tbody class="table-body">
+                                <?php
+                                    $count = sizeof($staffs['data']);
+                                    //echo $staffs['data'];
+                                    if($count > 0)
+                                    {
+                                    ?>
+                                        <?php  foreach($staffs['data'] as $staff) 
+                                        {  
+                                        ?>
                                     <tr>
-                                        <th class="text-column-emphasis" scope="row">3434</th> 
-                                        <th class="text-column" scope="row">Admin1</th> 
-                                        <th class="text-column" scope="row">Sarah</th> 
-                                        <th class="text-column" scope="row">Female</th> 
-                                        <th class="text-column" scope="row">0923838456</th> 
-                                        <th class="text-column" scope="row">District 5, HCM</th> 
-                                        <!-- <th class="text-column" scope="row">23/1/2001</th>  -->
+                                        <th class="text-column-emphasis" scope="row"><?php echo $staff['ID_User']?></th>
+                                        <th class="text-column" scope="row"><?php echo $staff['Username']?></th>
+                                        <th class="text-column" scope="row"><?php echo $staff['Fullname']?></th> 
+                                        <?php if($staff['Gender'] = 'F')
+                                            {?>
+                                                <th class="text-column" scope="row">Female</th> 
+                                            <?php
+                                            }
+                                            else
+                                            {
+                                            ?>
+                                                <th class="text-column" scope="row">Male</th> 
+                                            <?php
+                                            }
+                                        ?>
+                                        <th class="text-column" scope="row"><?php echo $staff['PhoneNumber']?></th> 
+                                        <th class="text-column" scope="row"><?php echo $staff['CurrAddress']?></th> 
                                         <th class="text-column" scope="row">
                                             <div class="text-column__action">
                                                 <button class="btn-control btn-control-delete" name="btn-delete">
                                                     <i class="fa-solid fa-trash-can btn-control-icon"></i>
                                                     Delete
                                                 </button>
-                                                <a href="update_staffs.php" class="btn-control btn-control-edit">
+                                                <a href="update_staffs.html" class="btn-control btn-control-edit">
                                                     <i class="fa-solid fa-user-pen btn-control-icon"></i>
                                                     Update
                                                 </a>
                                             </div>
                                         </th> 
                                     </tr>
+                                    <?php
+                                        }
+                                    }
+                                    else
+                                    {?>
+                                       <th class="text-column" scope="row"><?php echo 'No Data Found'?></th> 
+                                    <?php    
+                                    }
+                                    ?>
                                 </tbody>
                             </table>
 
