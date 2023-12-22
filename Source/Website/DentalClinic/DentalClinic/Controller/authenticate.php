fa2a8f4edd66afe86001eddbbbdcb7d3580b4edc
BNKieuNhi <93073321+BNKieuNhi@users.noreply.github.com>
2023-12-22 18:09:28 +0700

    Update

--- Source/Website/DentalClinic/DentalClinic/Controller/authenticate.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    if(!isset($_SESSION['authenticated']))
-    {
-        $_SESSION['status'] = 'Please login to access!';
-        header("location: ../login.php");
-        exit(0);
-    }
-?>
