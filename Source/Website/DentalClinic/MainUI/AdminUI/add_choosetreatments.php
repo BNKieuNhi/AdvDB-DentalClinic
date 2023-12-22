00cef734eed7bbadffe0b5367c6486a962e1e332
BNKieuNhi <93073321+BNKieuNhi@users.noreply.github.com>
2023-12-22 10:58:25 +0700

    Create add_choosetreatments.php

---
+++ Source/Website/DentalClinic/MainUI/AdminUI/add_choosetreatments.php
@@ -0,0 +1,80 @@
+<?php
+require_once('./partials/_head.php');
+?>
+
+<body>
+    <!-- Sidebar -->
+    <?php
+    require_once('./partials/_sidebar.php');
+    ?>
+
+    <!-- Main content -->
+    <div class="main-content">
+        <div class="content">
+            <!-- Top navbar -->
+            <?php
+            require_once('./partials/_topnav.php');
+            ?>
+            <!-- Page content -->
+            <div class="container">
+                <div class="container-recent">
+                    <div class="card shadow">
+                        <div class="container-recent__heading">
+                            <p class="recent__heading-title">Please Fill All Fields</p>
+                        </div>
+                        
+                        <div class="container-recent__body card__body-form">
+                            <form method="POST" class="">
+                                <div class="form-row">
+                                    <div class="form-row__flex">
+                                        <div class="form-col">
+                                            <label for="" class="form-col__label">Select Id</label>
+                                            <input type="text" name="select_id" class="form-control" readonly value>
+                                        </div>
+
+                                        
+                                        <div class="form-col">
+                                            <label for="" class="form-col__label">Treatment Id</label>
+                                            <select name="treatment_id" id="treatmentId" class="form-cotrol" onchange="getTreatment(this.value)">
+                                                <option value="" class="">1</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <hr class="navbar__divider">
+
+                                <div class="form-row">
+                                    <div class="form-row__flex">
+                                        <div class="form-col">
+                                            <label for="" class="form-col__label">Price ($)</label>
+                                            <input type="text" name="choosetreatment_price" class="form-control" value>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <br class="">
+
+                                <div class="form-row">
+                                    <div class="form-col margin-0">
+                                        <div class="form-col-bottom">
+                                            <input type="submit" name="addChooseTreatment" value="Add Choose Treatment" class="btn-control btn-control-add" value="">
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <!-- Footer -->
+            <?php 
+            require_once('./partials/_footer.php'); 
+            ?>
+        </div>
+    </div>
+
+</body>
+</html>
