fa86a8d13d282aec9006420bbd0a8c604c5022f1
BNKieuNhi <93073321+BNKieuNhi@users.noreply.github.com>
2023-12-23 23:58:40 +0700

    Update medicines.php

--- Source/Website/DentalClinic/MainUI/AdminUI/medicines.php
+++ Source/Website/DentalClinic/MainUI/AdminUI/medicines.php
@@ -61,7 +61,7 @@
                                 if(isset($_POST["btn-search"]))
                                 {
                                     $strKeyword = $_POST["search_text"];
-                                    $medicines = searchByKeyword('MEDICINE', 'MedicineName', $strKeyword);
+                                    $medicines = searchByKeyword('MEDICINE', $strKeyword);
                                 }
                                 else
                                 {
@@ -69,7 +69,7 @@
                                 }
                             ?>
                             <div class="container__heading-search">
-                                <input type="text" class="heading-search__area" placeholder="Search by name" name="search_text" value="<?php echo $strKeyword;?>">
+                                <input type="text" class="heading-search__area" placeholder="Search by name, price" name="search_text" value="<?php echo $strKeyword;?>">
                                 <button class="btn-control btn-control-search" name="btn-search">
                                     <i class="fa-solid fa-magnifying-glass btn-control-icon"></i>
                                     Search
